c610761d3c9ce4d3d55840eff565468fe6809233
Matheus Fogiatto <matheus.fogiattos@gmail.com>
2019-04-08 15:59:37 -0300

    detail

---
+++ AXApp/Controller/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  AXApp
+//
+//  Created by Matheus Fogiatto on 08/04/19.
+//  Copyright © 2019 Rovane Moura. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    var info: InfoModel?
+    
+    @IBOutlet weak var detailTitle: UILabel!
+    @IBOutlet weak var infoImage: UIImageView!
+    @IBOutlet weak var fullDescription: UILabel!
+    @IBOutlet weak var about: UILabel!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var location: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.detailTitle.text = info?.title
+        self.fullDescription.text = info?.fullDescription
+        self.infoImage.image = UIImage(named: info?.imageName ?? "")
+        self.about.text = info?.about
+        self.time.text = info?.time
+        self.location.text = info?.location
+    }
+    
+
+}
